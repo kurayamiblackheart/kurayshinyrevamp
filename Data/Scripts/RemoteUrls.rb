8d003d0c27f903451aa1de9151d8ddf214d6a521
kurayamiblackheart <kurayamiblackheart@gmail.com>
2023-09-25 18:11:18 +0200

    Kurayami did some shenanigans.

--- Data/Scripts/RemoteUrls.rb
+++ Data/Scripts/RemoteUrls.rb
@@ -1,6 +1,6 @@
 
 module Settings
-  SHINY_POKEMON_CHANCE = 16
+  # SHINY_POKEMON_CHANCE = 16 #no? what's the point?
   CREDITS_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/sprites/main/Sprite Credits.csv"
   SPRITES_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/infinitefusion-e18/main/Data/CUSTOM_SPRITES"
   VERSION_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/infinitefusion-e18/main/Data/VERSION"
